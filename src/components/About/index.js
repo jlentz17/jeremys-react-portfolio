584cc6c805dbb519aade8093591832d3cf632a27
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-09-10 15:29:20 -0700

    started on nav component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,25 @@
 import React from "react";
-import coverImage from "../../assets/cover/background-img-portfolio.jpeg"
+import coverImage from "../../assets/cover/background-img-portfolio.jpeg";
 
 function About() {
   return (
     <section className="my-5">
       <h1 id="about">Hey, I'm Jeremy!</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%"}} alt="cover photo"/>
+      <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover photo"
+      />
+      <div className="my-2">
+        <p>
+          Full-Stack or software developer with a background in business and
+          sales. Employees troubleshooting skills, and an eye for detail in the
+          pursuit of developing engaging apps. Works well both independently and
+          in a team environment. Passionate, flexible and an excellent
+          communicator eager to learn.
+        </p>
+      </div>
     </section>
   );
 }
