da00bd234d16112ad748440583316bbcea19df11
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-09-13 08:14:29 -0700

    added cursive to my name

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Nav() {
-  return <header></header>;
+  return (
+    <header>
+      <h2>
+        <a href="/">
+          <span id="name-logo" role="img" aria-label="logo">
+            Jeremy Lentz
+          </span>
+        </a>
+      </h2>
+    </header>
+  );
 }
 
 export default Nav;
