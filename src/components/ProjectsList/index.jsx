d0b9b3d918e2b778359ca69680b99ce6bda014b7
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2022-06-27 15:33:55 -0700

    fixed format and spacing

--- src/components/ProjectsList/index.jsx
+++ src/components/ProjectsList/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./projectsList.scss";
+import { Container, Row, Col } from 'react-bootstrap';
 
 function ProjectsList() {
   return (
@@ -10,10 +11,10 @@ function ProjectsList() {
         </div>
       </div>
       <div className="right">
-        <div className="wrapper">
-          <h2>About me:</h2>
-          <h1>Born in Phoenix, AZ</h1>
-          <h4>
+        <Row className="wrapper">
+          <Col><h2>About me:</h2></Col>
+          <Col><h1>Born in Phoenix, AZ</h1></Col>
+          <Col> <h4>
           Full-stack Software developer with a background in music business, performance, and sales. Detail-oriented professional with strong problem-solving skills and a proven ability to manage multiple projects while meeting challenging deadlines.
           Earned a certificate in full stack development from the University of Arizona (October 2021), and worked as Software Engineer I for a year with newly developed skills in JavaScript, CSS, React.js, Ruby on Rails, and responsive web design. <br /><br/>At my previous job I:
           </h4>
@@ -25,14 +26,14 @@ function ProjectsList() {
             <br />
             <li>
               Developed screen views, navigation, and flow of web application using React, based on Adobe XD designs, to implement and deploy to production, while maintaining and updating our legacy applications running on Ruby on Rails and NextJS.
-              
+
             </li>
             <br />
             <li>
-              Produced multiple visual elements of web applications by translating UI/UX design wireframes into code within the deadline given
+              Produced multiple visual elements of web applications by translating UI/UX design wireframes into code within the deadline given.
             </li>
-          </ul>
-        </div>
+          </ul></Col>
+        </Row>
         <a href="#projects">
           <img src="assets/down.png" alt="down arrow" />
         </a>
